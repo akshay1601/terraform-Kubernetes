b80ee247a24e9dcbea8bcc0fbc36bfa7efd61412
Akshay Sudhakar Virkhare <akshay.sudhakarvirkhare@rackspace.com>
2025-04-01 01:00:05 +0530

    Chnaging terraform files and adding ssl

--- Infrastructure/cloudformation/ssl-certificate/acm.tf
+++ Infrastructure/cloudformation/ssl-certificate/acm.tf
@@ -13,12 +13,12 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
-data "aws_route53_zone" "example" {
+data "aws_route53_zone" "read-domain_name" {
   name         = var.route-name
   private_zone = false
 }
 
-resource "aws_route53_record" "example" {
+resource "aws_route53_record" "write-domain_name" {
   for_each = {
     for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
@@ -32,11 +32,11 @@ resource "aws_route53_record" "example" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = data.aws_route53_zone.example.zone_id
+  zone_id         = data.aws_route53_zone.read-domain_name.zone_id
 }
 
 resource "aws_acm_certificate_validation" "example" {
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.example : record.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.write-domain_name : record.fqdn]
 }
 
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,10 +1,12 @@
-#Client varibles
+#Api  varibles
 namespace = "development"
 hash_key = "_id"
 write = 5
 read = 5
 type = "S"
+
+#Infrastructure SSL variables
+environment = "development"
 domain_name = "*.637423197349.realhandsonlabs.net"
 subject_alternative_names  = ["637423197349.realhandsonlabs.net"]
-environment = "development"
 route-name = "637423197349.realhandsonlabs.net"

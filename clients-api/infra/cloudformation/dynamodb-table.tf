606a0b8867bd88d706c887c7670f6187a9e4d1dc
Akshay Sudhakar Virkhare <akshay.sudhakarvirkhare@rackspace.com>
2025-03-31 15:15:19 +0530

    Chnaging terraform files

--- clients-api/infra/cloudformation/dynamodb-table.tf
+++ clients-api/infra/cloudformation/dynamodb-table.tf
@@ -1,6 +1,8 @@
 resource "aws_dynamodb_table" "example" {
   name             = "${var.namespace}-clients"
   hash_key         = var.hash_key
+  write_capacity   = var.write
+  read_capacity    = var.read 
   
 
 attribute {
@@ -10,3 +12,4 @@ attribute {
   }
 
 }
+

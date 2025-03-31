5e3f89383d016d66cf792ef2bcad1a21d23be98d
Akshay Sudhakar Virkhare <akshay.sudhakarvirkhare@rackspace.com>
2025-03-31 17:28:22 +0530

    Chnaging terraform files and adding ssl

--- Infrastructure/cloudformation/ssl-certificate/acm.tf
+++ Infrastructure/cloudformation/ssl-certificate/acm.tf
@@ -1,26 +1,14 @@
-# resource "aws_acm_certificate" "cert" {
-#   domain_name       = var.domain_name
-#   validation_method = "DNS"
+resource "aws_acm_certificate" "cert" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
   
 
-#   tags = {
-#     Environment = var.environment
-#   }
-
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
-
-resource "aws_acm_certificate" "cert" {
-  domain_name       = "992382478305.realhandsonlabs.net"
-  validation_method = "DNS"
-
   tags = {
-    Environment = "development"
+    Environment = var.environment
   }
 
   lifecycle {
     create_before_destroy = true
   }
 }
+

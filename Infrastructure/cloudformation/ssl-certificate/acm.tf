80e825237bd54597958a549400fe983301f69c68
Akshay Sudhakar Virkhare <akshay.sudhakarvirkhare@rackspace.com>
2025-03-31 22:37:50 +0530

    Chnaging terraform files and adding ssl

--- Infrastructure/cloudformation/ssl-certificate/acm.tf
+++ Infrastructure/cloudformation/ssl-certificate/acm.tf
@@ -36,7 +36,7 @@ resource "aws_route53_record" "example" {
 }
 
 resource "aws_acm_certificate_validation" "example" {
-  certificate_arn         = aws_acm_certificate.example.arn
+  certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for record in aws_route53_record.example : record.fqdn]
 }
 

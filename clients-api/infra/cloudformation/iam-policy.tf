40bfd4d553ada8a80f58cba6c2161d3e129a8bff
Akshay Sudhakar Virkhare <akshay.sudhakarvirkhare@rackspace.com>
2025-04-01 15:31:33 +0530

    Chnaging terraform files and adding ssl

--- clients-api/infra/cloudformation/iam-policy.tf
+++ clients-api/infra/cloudformation/iam-policy.tf
@@ -1,4 +1,4 @@
 resource "aws_iam_policy" "clients-api" {
   name = "ClientsApiPolicy"
-  policy = "${file("iam-policy.json")}"
+  policy = "${file("./clients-api/infra/cloudformation/iam-policy.json")}"
 }

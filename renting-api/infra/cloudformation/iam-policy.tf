0af785b983720c1dd55b2f40a9cd646135759826
Akshay Sudhakar Virkhare <akshay.sudhakarvirkhare@rackspace.com>
2025-04-01 21:02:21 +0530

    Chnaging terraform files and adding ssl

---
+++ renting-api/infra/cloudformation/iam-policy.tf
@@ -0,0 +1,44 @@
+locals {
+  name_suffix_name = "${var.namespace != "" ? var.namespace : var.namespace }"
+}
+
+locals {
+  name_suffix_account = "${var.aws-account != "" ? var.aws-account : var.aws-account}"
+}
+
+locals {
+  name_suffix_region = "${var.aws-region != "" ? var.aws-region : var.aws-region}"
+}
+
+resource "aws_iam_policy" "clients-api" {
+  name = "${replace(local.name_suffix_name, "-", "")}-RentingApiPolicy"
+
+#policy = "${file("./clients-api/infra/cloudformation/iam-policy.json")}"
+policy = <<EOT
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "dynamodb:BatchGetItem",
+                "dynamodb:PutItem",
+                "dynamodb:DescribeTable",
+                "dynamodb:DeleteItem",
+                "dynamodb:GetItem",
+                "dynamodb:Scan",
+                "dynamodb:Query",
+                "dynamodb:UpdateItem"
+            ],
+            "Resource": [
+                "arn:aws:dynamodb:${(local.name_suffix_region)}:${replace(local.name_suffix_account, "-", "")}:table/${replace(local.name_suffix_name, "-", "")}-renting",
+                "arn:aws:dynamodb:${(local.name_suffix_region)}:${replace(local.name_suffix_account, "-", "")}:table/${replace(local.name_suffix_account, "-", "")}/index/*"
+        
+            ],
+            "Effect": "Allow"
+        }
+    ]
+ }
+
+EOT
+}
+

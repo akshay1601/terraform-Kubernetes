f8193dec4c016bdb03b31478a809101bba60b0ab
Akshay Sudhakar Virkhare <akshay.sudhakarvirkhare@rackspace.com>
2025-04-01 15:43:06 +0530

    Chnaging terraform files and adding ssl

--- clients-api/infra/cloudformation/iam-policy.tf
+++ clients-api/infra/cloudformation/iam-policy.tf
@@ -1,4 +1,29 @@
 resource "aws_iam_policy" "clients-api" {
   name = "ClientsApiPolicy"
-  policy = "${file("./clients-api/infra/cloudformation/iam-policy.json")}"
+#   policy = "${file("./clients-api/infra/cloudformation/iam-policy.json")}"
+policy = <<EOT
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "dynamodb:BatchGetItem",
+                "dynamodb:PutItem",
+                "dynamodb:DescribeTable",
+                "dynamodb:DeleteItem",
+                "dynamodb:GetItem",
+                "dynamodb:Scan",
+                "dynamodb:Query",
+                "dynamodb:UpdateItem"
+            ],
+            "Resource": [
+                "arn:aws:dynamodb:us-east-1:654654152817:table/development-clients",
+                "arn:aws:dynamodb:us-east-1:654654152817:table/654654152817/index/*"
+            ],
+            "Effect": "Allow"
+        }
+    ]
+ }
+EOT
 }
+
